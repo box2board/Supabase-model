1e4b09af7a20ef1d1ad946b55505bd589ae8f144
box2board <ashdchristie@gmail.com>
2025-10-27 13:13:47 -0400

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// This configuration tells PostCSS to use Tailwind CSS and Autoprefixer.
+// See https://tailwindcss.com/docs/installation#postcss for details.
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
