29ce601744ea5a75ee0285236b6010f13ebd4a68
box2board <ashdchristie@gmail.com>
2025-10-28 22:55:43 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  // No custom experimental keys needed for now
 };
+
 module.exports = nextConfig;
