5739d756af6e39a026460b4d6f1ce01f1b3891b5
box2board <ashdchristie@gmail.com>
2025-10-28 23:10:08 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // Enable class-based dark mode. This allows us to toggle dark styles by
-  // setting the `dark` class on the html element in the root layout.
-  darkMode: 'class',
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      // You can extend the default Tailwind theme here. For now we're
-      // leaving it empty so the default palette (including grays) is available.
-    },
+    extend: {},
   },
   plugins: [],
 };
