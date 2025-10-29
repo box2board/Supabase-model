052249ab728d114dc8cb364fedeb9b7d5c721664
box2board <ashdchristie@gmail.com>
2025-10-28 23:09:39 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,3 @@
-// This configuration tells PostCSS to use Tailwind CSS and Autoprefixer.
-// See https://tailwindcss.com/docs/installation#postcss for details.
 module.exports = {
   plugins: {
     tailwindcss: {},
