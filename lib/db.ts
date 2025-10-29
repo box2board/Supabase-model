78a4d1f6977111a54cc5520272d22035e8248259
box2board <ashdchristie@gmail.com>
2025-10-28 22:53:18 -0400

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,11 @@
-// Simple Prisma client singleton.
-// Requires @prisma/client to be installed in package.json.
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ log: ['error', 'warn'] });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"],
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
