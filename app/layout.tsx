0353ce0667c1694b45d493b191d3c49d494fe370
box2board <ashdchristie@gmail.com>
2025-10-28 22:50:09 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "Box2Board",
+  description: "Sports analytics, simplified.",
+};
+
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
