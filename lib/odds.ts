7dd2cf5c26749e5c6d633e18ec2c813731d8987c
box2board <ashdchristie@gmail.com>
2025-11-03 10:35:26 -0500

    Delete lib/odds.ts

--- lib/odds.ts
+++
@@ -1,9 +0,0 @@
-export async function fetchOdds() {
-  // TODO: call The Odds API with your key and return normalized data
-  return [];
-}
-
-export function extractOddsSnapshots(_odds: unknown[]) {
-  // TODO: convert odds into your snapshot format
-  return [];
-}
